775f680a7e3cc82b124f1ed7f2168ef947b0b720
Robert Rogers <rerogers@gmail.com>
2023-04-23 18:50:23 -0500

    updated a conversation between kanye and ron swanson

---
+++ APIsAndJSON/CreateQuote.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
+{
+    internal class CreateQuote
+    {
+        private HttpClient _client;
+
+        public CreateQuote(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            //Console.WriteLine($"Kanye: {kanyeQuote}\n");
+            return kanyeQuote;
+        }
+
+        public string RonQuote()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            //Console.WriteLine($"Ron Swanson:  {ronQuote}\n");
+            return ronQuote;
+        }
+    }
+}
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var client = new HttpClient();
+            var quote = new CreateQuote(client);
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: {quote.KanyeQuote()}\n");
+                Console.WriteLine($"Ron Swanson: {quote.RonQuote()}\n");
+            }
         }
     }
 }

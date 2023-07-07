0b77e64fcebd0a73e373337bc5e575db1e85acfa
Robert Rogers <rerogers@gmail.com>
2023-07-07 15:41:01 -0500

    updated stuff

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -8,21 +8,21 @@ namespace APIsAndJSON
         {
 
             #region RonVSKanye
-            //var client = new HttpClient();
-            //var quote = new RonVSKanyeAPI(client);
-            //Console.WriteLine("How many time do you want Ron to talk?");
-            //int num = Convert.ToInt32(Console.ReadLine());
-
-            //for (var i = 0; i < num; i++)
-            //{
-            //    Console.WriteLine($"Kanye: {quote.KanyeQuote()}\n");
-            //    Console.WriteLine($"Ron Swanson: {quote.RonQuote()}\n");
-            //}
-            #endregion
             var client = new HttpClient();
-            var weather = new OpenWeatherMapAPI(client);
+            var quote = new RonVSKanyeAPI(client);
+            Console.WriteLine("How many time do you want Ron to talk?");
+            int num = Convert.ToInt32(Console.ReadLine());
 
-            weather.WeatherAPI();
+            for (var i = 0; i < num; i++)
+            {
+                Console.WriteLine($"Kanye: {quote.KanyeQuote()}\n");
+                Console.WriteLine($"Ron Swanson: {quote.RonQuote()}\n");
+            }
+            #endregion
+            //var client = new HttpClient();
+            //var weather = new OpenWeatherMapAPI(client);
+
+            //weather.WeatherAPI();
         }
     }
 }
